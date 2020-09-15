dd9bdc53be7af8cc62f054fcd6a2e6f30da27687
Ali Rahhal <arahhal997@gmail.com>
2020-09-15 22:37:56 +0300

    Upate vm_test.go

--- lib/vm/vm_test.go
+++ lib/vm/vm_test.go
@@ -1,7 +1,47 @@
 package vm
 
-import "testing"
+import (
+	"golanglox/lib/chunk"
+	"testing"
+)
 
-func Test(t *testing.T) {
+func createChunkForTesting(bytes ...byte) *chunk.Chunk {
+	c := chunk.New()
+	for _, b := range bytes {
+		c.WriteChunk(b, 1)
+	}
+	return c
+}
 
+func TestReadByte(t *testing.T) {
+	// test for reading the current chunk byte
+	t.Run("read current chunk byte", func(t *testing.T) {
+		v := New()
+		v.InitVM()
+
+		c := createChunkForTesting(1, 2, 3)
+
+		v.Chunk = c
+		v.IP = &(v.Chunk.Code[0])
+
+		if v.readByte() != 1 {
+			t.Errorf("vm.readByte() failed, expected to read the current byte")
+		}
+	})
+
+	// test for incrementing the instruction pointer(IP)
+	t.Run("increment instruction pointer(IP)", func(t *testing.T) {
+		v := New()
+		v.InitVM()
+
+		c := createChunkForTesting(1, 2, 3)
+
+		v.Chunk = c
+		v.IP = &(v.Chunk.Code[0])
+
+		v.readByte()
+		if v.readByte() != 2 {
+			t.Errorf("vm.readByte() failed, expected to increment instruction pointer(IP)")
+		}
+	})
 }
